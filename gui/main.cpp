4ed8da849882e630163aa1cced5d48816537c538
Harry Karvonen <harry.karvonen@gmail.com>
2013-03-06 21:16:35 +0200

    Mikäli Tapahtuman valinnassa painetaan esc, eli ei valita mitään tapahtumaa., niin suljetaan ohjelma.

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,11 +1,23 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
 
+#include "type/tapahtuma.h"
+
+#include "makrot.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    QApplication::setOrganizationName(_("PispalanInsinööritoimistoOy"));
+    QApplication::setApplicationName(_("Tulospalvelu"));
+
+    // Tarkistetaan valittiinko tapahtuma vai painettiinko esc tai muuta.
+    if (Tapahtuma::tapahtuma()->id() == 0) {
+        return 0;
+    }
 
     return a.exec();
 }
